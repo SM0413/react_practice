3a6656d18eecc0ab72ba39a84ebcec131a5c9297
SM0413 <kbjtmdals@naver.com>
2022-08-26 10:10:41 +0900

    Nested Routes part Two

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { Link, Route, Routes, useLocation, useParams } from "react-router-dom";
+import {
+  Link,
+  Route,
+  Routes,
+  useLocation,
+  useMatch,
+  useParams,
+} from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 import { Chart } from "./Chart";
 import { Price } from "./Price";
@@ -71,6 +78,27 @@ const Description = styled.p`
   margin: 20px 0px;
 `;
 
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
 interface IPrams {
   coinId: string;
 }
@@ -141,6 +169,8 @@ export function Coin() {
   const state = location.state as IRouteState;
   const [info, setInfo] = useState<IInfoData>();
   const [price, setPrice] = useState<IPriceData>();
+  const priceMatch = useMatch(":coinId/price");
+  const chartMatch = useMatch(":coinId/chart");
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -199,6 +229,14 @@ export function Coin() {
               <span>{price?.max_supply}</span>
             </OverviewItem>
           </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={"chart"}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={"price"}>Price</Link>
+            </Tab>
+          </Tabs>
           <Routes>
             <Route path={"price"} element={<Price />} />
             <Route path={"chart"} element={<Chart />} />
