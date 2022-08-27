ace9bf4c377fbb12b2f84327e38ab0f641bf6941
SM0413 <kbjtmdals@naver.com>
2022-08-27 15:01:20 +0900

    Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api/coinAPI";
-
+import ApexChart from "react-apexcharts";
 interface IPropsType {
   coinId: string;
 }
@@ -16,10 +16,43 @@ interface ICoinHistory {
   market_cap: number;
 }
 export function Chart({ coinId }: IPropsType) {
-  const { isLoading, data } = useQuery<ICoinHistory>(
+  const { isLoading, data } = useQuery<ICoinHistory[]>(
     ["Histroy Of", coinId],
     () => fetchCoinHistory(String(coinId))
   );
 
-  return <div>{isLoading ? "Loading...." : "Chart"}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading...."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            { name: "price", data: data?.map((price) => price.close) as [] }, //data에 값이 있다면 price.close값을 data에 값이 없다면 [](빈값)값을 입력한다.
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 1,
+            },
+            xaxis: {
+              labels: { show: false },
+            },
+            yaxis: {
+              show: false,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
