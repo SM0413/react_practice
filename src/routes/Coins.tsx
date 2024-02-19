90cd8c12c6bdd144e577ec83b1101443e5e216d2
SM0413 <110504277+SM0413@users.noreply.github.com>
2024-02-19 15:38:37 +0900

    hover 삭제

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,3 @@
-import { hover } from "@testing-library/user-event/dist/hover";
 import { Helmet } from "react-helmet";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
